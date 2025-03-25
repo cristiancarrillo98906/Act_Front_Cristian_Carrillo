fa69fdb698f7f46eb02f823d2ef8bd470810257c
cristian carrillo <cristiancarrillo98906@gmail.com>
2025-03-25 17:46:45 +0100

    Front get y getByID - OK TERMINADO

--- app.js
+++ app.js
@@ -42,13 +42,15 @@ function cargarUsuarios(){
             }
             detallesCell.appendChild(detallesButton)
 
+
             row.appendChild(idCell)
             row.appendChild(nombreCell)
             row.appendChild(apellidoCell)
-            row.appendChild(correoCell)
+            //row.appendChild(correoCell)
             row.appendChild(usuarioCell)
-            row.appendChild(contraseñaCell)
-            row.appendChild(perfilCell)
+            //row.appendChild(contraseñaCell)
+            //row.appendChild(perfilCell)
+            row.appendChild(detallesCell)
             tbody.appendChild(row)
         }
     })
@@ -58,4 +60,60 @@ function cargarUsuarios(){
     })
 }
 
+
+function cargarRestaurantes(){
+    fetch("http://localhost:4050/restaurantes")
+    .then(function(respuestaTextoPlano){
+        //La respuesta (OK o KO) la transformo en JSON
+        return respuestaTextoPlano.json()
+    })
+    .then(function(data){
+        //Gestionamos el OK con "data" que tendrá formato JSON
+        //console.log(data)
+        const tbody = document.getElementById("restaurantes-body")
+        for(let restaurantes of data){
+            //Para cada producto, creamos una fila
+            const row = document.createElement("tr") //<tr></tr>
+            const idCell = document.createElement("td")
+            idCell.innerText = restaurantes.id
+
+            const nombreCell = document.createElement("td")
+            nombreCell.innerText = restaurantes.nombre
+
+            const ubicacionCell = document.createElement("td")
+            ubicacionCell.innerText = restaurantes.ubicacion
+
+            const descripcionCell = document.createElement("td")
+            descripcionCell.innerText = restaurantes.descripcion
+
+            const propietarioCell = document.createElement("td")
+            propietarioCell.innerText = restaurantes.propietario
+
+            //SHOW BUTTON
+            const detallesCell = document.createElement("td")
+            const detallesButton = document.createElement("button")
+            detallesButton.innerText = "Ver"
+            detallesButton.classList.add("btn-detalles"); 
+            detallesButton.onclick = function(){
+                window.location.href = "showRestaurante.html?id=" + restaurantes.id
+            }
+            detallesCell.appendChild(detallesButton)
+
+
+            row.appendChild(idCell)
+            row.appendChild(nombreCell)
+            row.appendChild(ubicacionCell)
+            
+            row.appendChild(detallesCell)
+            tbody.appendChild(row)
+        }
+    })
+    .catch(function(error){
+        //Gestionamos el KO con "error" que tendrá formato JSON
+        console.log("Error al obtener los módulos: ", error)
+    })
+}
+
+// MAIN - Ejecución:
 cargarUsuarios()
+cargarRestaurantes()
